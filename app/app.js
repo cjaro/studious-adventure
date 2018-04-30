63293efd778ae4c86ca9bca8dfb101aef3fff83c
Catherine Jarocki <cjarocki@gmail.com>
2018-04-30 11:14:37 -0500

    studious adventure 2

---
+++ app/app.js
@@ -0,0 +1,38 @@
+var app = angular.module('ggApp', ['ngRoute']);
+
+app.config(function($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl : 'views/home.html',
+                controller  : 'MainController'
+            })
+            .when('/about', {
+                templateUrl : 'views/about.html',
+                controller  : 'AboutController'
+            })
+            .when('/work', {
+              templateUrl: '/views/work.html',
+              controller: 'WorkController'
+            })
+            .when('/contact', {
+                templateUrl : 'views/contact.html',
+                controller  : 'ContactController'
+            });
+    });
+
+    // create the controller and inject Angular's $scope
+    app.controller('MainController', function($scope) {
+        // create a message to display in our view
+        $scope.message = 'Everyone come and see how good I look!';
+    });
+    app.controller('AboutController', function($scope) {
+        $scope.message = 'Look! I am an about page.';
+    });
+    app.controller('WorkController', function($scope) {
+        $scope.message = 'ayo I am an work/resume page.';
+    });
+    app.controller('ContactController', function($scope) {
+        $scope.message = 'Contact us! fuck you.';
+    });
+
+});
